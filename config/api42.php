fc8de44ef52697d25e5ab247da6c06b0a4a2ca8a
DrimZ <qlebevillon@gmail.com>
2016-09-15 22:34:35 +0200

    added gitignore

--- config/api42.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$API_UID = "eb594afb4cd3e02e958c83eb638dbba332cbc3ad54cf777620919a9daef50e33";
-$API_SECRET = "25bcef8994ed97b4dab4ffc7a2d572370943bc6e7c5a5a30c4ca5368ece8ac47";
-
-?>
--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-
-$DB_DSN = "mysql:dbname=camagru;host=localhost";
-$DB_USER = "";
-$DB_PASSWORD = "";
-
-?>
